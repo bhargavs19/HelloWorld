c29560871a526fb9bb3e9b0c7dac8586ea03f99f
root <root@ip-172-31-31-228.ap-south-1.compute.internal>
2018-09-18 02:55:33 +0000

    NewLine Added

--- src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -9,6 +9,7 @@ public class HelloWorldServlet extends HttpServlet
   public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
     PrintWriter out = res.getWriter();
     out.println( "Hello, Good mORNING nani" );
+    out.println( "Hello, Trying GitHook Project" );
     out.close();
   }
 }
