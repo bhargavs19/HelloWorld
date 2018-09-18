e17acb2c0443c5fc9de16e57d7ac686aa2a5fa0b
root <root@ip-172-31-31-228.ap-south-1.compute.internal>
2018-09-18 03:07:08 +0000

    NewLine comment

--- src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class HelloWorldServlet extends HttpServlet 
 {
   public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
-#    PrintWriter out = res.getWriter();
+    PrintWriter out = res.getWriter();
     out.println( "Hello, Good mORNING nani" );
     out.println( "Hello, Trying GitHook Project" );
     out.close();
